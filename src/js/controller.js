ea8f99848a72195a4e057e62b819acbe7e1fbd01
Nikolay Yakimov <nyakimov123@gmail.com>
2025-02-24 16:10:03 +0200

    add update functionality in the , so that we don't need to re-render the views again

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,10 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+
+    //Update results view to mark selected search result
+    resultView.update(model.getSearchResultsPage());
+
     //Load recipe
     await model.loadRecipe(id);
 
@@ -63,7 +67,8 @@ const controlServings = function (newServings) {
   //Update the recipe servings (in state)
   model.updateServings(newServings);
   //Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 //Implement publisher-subscribe pattern
